76de7c8ad59bf7346d0d99b5b59c24bd3fb4f3a6
SnacBerry <info@snacberry.com>
2026-05-27 12:22:03 +0530

    Create SnacBerryApplication.java

---
+++ src/main/java/com/snacberry/SnacBerryApplication.java
@@ -0,0 +1,11 @@
+package com.snacberry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnacBerryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SnacBerryApplication.class, args);
+    }
+}
